2dac37f8eba0b45598d5308284b87cac505797a1
Raniya Javed <raniyajaved4@gmail.com>
2025-09-19 08:26:10 +0530

    updated files after some changes

--- db.js
+++ db.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 // Define the MongoDB connection URL
-//const mongoURL = 'mongodb://localhost:27017/voting' // Replace 'mydatabase' with your database name
-const mongoURL = 'mongodb+srv://raniyajaved4:QWERTY2025@voting.gm5etfp.mongodb.net/'
+//const mongoURL = 'process.env.MONGODB_URL_LOCAL' // Replace 'mydatabase' with your database name
+const mongoURL = process.env.ONLINE_MONGODB_URL
 
 // Set up MongoDB connection
 mongoose.connect(mongoURL, {
